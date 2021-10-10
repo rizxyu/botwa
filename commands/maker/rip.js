e009861f432fcfc7f48fd228386100a2dce687cc
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-10 10:20:33 +0700

    Update rip.js

--- commands/maker/rip.js
+++ commands/maker/rip.js
@@ -1,1 +1,17 @@
+const fs = Ft.fs
+const { canvas } = require("canvacord")
 
+module.exports = {
+name: ["rip"],
+type: ["maker"],
+useLimit: true,
+description: "make Rip picture",
+utilisation: userbot.prefix + "rip",
+
+async execute(m) {
+const name = conn.getName(m.sender)
+
+canvas.rip(await conn.getProfilePicture(m.sender)).then((p) => 
+conn.sendButImg(m.chat, p, `Telah meninggal ${conn.getName(m.sender)}`, `Botol`, `innalilahi`, `innalilahi`, m))
+}
+}
