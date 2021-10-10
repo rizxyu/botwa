ce169da607aabe0a8988dc3128ca8f1b13106312
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-10 10:49:53 +0700

    Create runc.js

---
+++ commands/core/runc.js
@@ -0,0 +1,23 @@
+module.exports = {
+async execute(m) {
+
+ let { conn, text } = data
+if (new Date() * 1 > 1000) {
+
+const uptime = process.uptime();
+
+conn.setStatus(`BOT AKTIF : ${count(uptime)} | BOT DIBUAT OLEH FEAR TEAM`)
+  }
+ }
+}
+
+function count(seconds){
+if (typeof seconds !== "number") throw "connError: Unexpected Param " + typeof seconds
+let hours = Math.floor(seconds / (60*60));
+let minutes = Math.floor(seconds % (60*60) / 60);
+let second = Math.floor(seconds % 60);
+return `${pad(hours)} Jam ${pad(minutes)} Menit ${pad(second)} Detik`
+}
+function pad(s) {
+return (s < 10 ? '0' : '') + s;
+}
