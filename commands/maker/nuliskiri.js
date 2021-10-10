0cac2acaacf77b2a9b6b3b476cbec7d25b596f4f
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-10 13:06:24 +0700

    Create nuliskiri.js

---
+++ commands/maker/nuliskiri.js
@@ -0,0 +1,21 @@
+const fs = Ft.fs
+const fetch = require('node-fetch')
+
+module.exports = {
+
+name: ["nuliskiri"],
+
+type: ["maker"],
+useLimit: true,
+description: "menulis dengan text",
+utilisation: userbot.prefix + "nuliskiri teks",
+
+async execute(m) {
+ let { conn, text } = data
+if (text) return m.reply('teksnya?')
+
+let res = await fetch(`https://hardianto.xyz/api/nuliskiri?text=${text}&apikey=hardianto`)
+
+conn.sendFile(m.chat, res, 'kertas.jpg', 'Sudah Jadi jangan lupa support bot kami ya', m)
+   }
+}
