3251d49b5300412abb2e45afc65d760a80e58d25
Rizky Adi N <88314302+Rizxyu@users.noreply.github.com>
2021-10-10 16:22:08 +0700

    Create Circle.js

---
+++ commands/maker/Circle.js
@@ -0,0 +1,18 @@
+const fs = Ft.fs
+const fetch = require("node-fetch")
+var { Canvas } = require("canvacord")
+
+module.exports = {
+name: ["circle"],
+type: ["maker"],
+useLimit: true,
+description: "make circle picture",
+utilisation: userbot.prefix + "circle",
+
+async execute(m) {
+let { conn } = data
+const name = conn.getName(m.sender)
+
+Canvas.circle(await conn.getProfilePicture(from)).then((p) => conn.sendFile(m.chat, p, 'pp.jpg', 'done', m))}
+}
+}
